40d66b0e8f03b938bb73635f8a8601ed4b190b56
songwening <swmyjx@sina.com>
2018-07-30 13:36:24 +0800

    小杨，修改了程序

--- app/src/main/java/com/dianbin/xz/MainActivity.java
+++ app/src/main/java/com/dianbin/xz/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+   String aa="我是小杨，我修改了程序";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
