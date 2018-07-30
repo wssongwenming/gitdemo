6a0787bf1ab5863510ee584df44e5b5ee2c410b1
songwening <swmyjx@sina.com>
2018-07-30 14:57:23 +0800

    小杨，添加了分支，添加了功能

--- app/src/main/java/com/dianbin/xz/MainActivity.java
+++ app/src/main/java/com/dianbin/xz/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
    String aa="我是小杨，我修改了程序";
    String bb="我是小杨，我创建了新分支，开发了新功能，估计有冲突";
-   String bb="我是小杨，我创建了新分支，开发了新功能，不知道有没有冲突";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
